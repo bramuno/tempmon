c8394401ffabdac82ca7fdc96a3b5096f5b37917
bramuno <bramuno@users.noreply.github.com>
2013-12-31 19:17:41 -0700

    Create myTemp.pl

---
+++ myTemp.pl
@@ -0,0 +1,210 @@
+#!/usr/bin/perl -w
+###########################################################################################
+
+#use lib "/usr/local/nagios/libexec";
+use lib "/usr/lib/nagios/plugins";
+use utils qw(%ERRORS);
+use Getopt::Long;
+use DBI;
+#use Mysql;
+use Math::Round;
+
+my $debug = "no";
+my $min = undef;
+my $max = undef;
+my $type = undef;
+my $folder = undef;   #define file name 
+my $standard = "";
+my $zone = undef;
+my $process = "cacti";
+
+# ####################################################################################################################################################################################
+sub print_usage {
+ print "/usr/bin/perl myTemp.pl -x <minimum> -y <maximum> -f <Folder Name> -t <C/F> -d <yes> \nExample: ./myTemp.pl -x 40 -y 100 -f folderName -t F -z zoneName -d yes\n";
+}
+# ####################################################################################################################################################################################
+sub help {
+print_usage();
+print <<EOT;
+	-h, --help
+		print this help message
+	-x, --minimum=value
+		Minimum temperature before sending alert(required)	
+	-y, --maximum=value
+		Maximum temperature before sending alert(required)			
+	-f, --folder=name
+		Folder name where temperature data is stored (required)	
+	-t, --type=C/F
+		Celcius of Farenheit (required)	
+	-z, --zone=name
+		Name of zone (or kennel)	
+	-d, --debug=yes
+		print data for command line debugging (optional)
+	-p, --process=nagios
+EOT
+}
+# ####################################################################################################################################################################################
+
+sub check_options {
+ Getopt::Long::Configure ("bundling");
+ GetOptions (
+	 'h'     => \$help,    'help'         		 =>      \$help,
+	 'x:s'     => \$minimum,    'minimum'          =>      \$minimum,
+	 'y:s'     => \$maximum,    'maximum'          =>      \$maximum,
+	 'f:s'     => \$folder,    'folder'        =>      \$folder,
+	 't:s'     => \$type,    'type'        =>      \$type,
+	 'z:s'     => \$zone,    'zone'        =>      \$zone,
+         'p:s'     => \$process,    'process'        =>    \$process,
+	 'd:s'   => \$debug,    'debug'        		 =>      \$debug		 
+	    );
+		
+if (defined ($help)) {
+	help();
+	exit $ERRORS{"UNKNOWN"}};
+if (!defined ($minimum)) {
+	print"No minimum temperature defined, use -x\n";
+	print_usage();
+	exit $ERRORS{"UNKNOWN"}};
+
+if (!defined($maximum)) {
+	print "No maximum temperature defined, use -y\n";
+        print_usage();
+        exit $ERRORS{"UNKNOWN"}};	
+if (!defined($folder)) {
+	print "No filename defined, use -f\n";
+        print_usage();
+        exit $ERRORS{"UNKNOWN"}};	
+if (!defined($type)) {
+	print "No temperature standard defined, use -t\n";
+        print_usage();
+        exit $ERRORS{"UNKNOWN"}}
+if (!defined($zone)) {
+	print "No zone defined, use -z\n";
+        print_usage();
+        exit $ERRORS{"UNKNOWN"}}
+		
+if($type eq "f" ){$type = "F";};if($type eq "c"){ $type = "C";};
+
+if ( $type eq "F" || $type eq "C"  ) {  
+		print ""; # do nothing
+	}else{
+	print "Unknown temperature standard defined.  Use C or F.\n";
+        print_usage();
+        exit $ERRORS{"UNKNOWN"}};	
+}
+sub CtoF { my $c = shift; $c =~ s/[^\d\.]//g; return (9/5)*($c+32); }
+
+# ###############################################################################################################################################################################
+check_options();
+# ###############################################################################################################################################################################
+my $date = ` date +"%Y%m%d"`;
+# ## MySQL Stuff ################################################################################################################################################################
+#$connect = Mysql->connect($lhost, $database, $user, $pw) || die "Connection to: $database failed $ERRORS{\"UNKNOWN\"}";
+#$connect -> selectdb($database);
+# ################################################################################################
+#my $query_current_previous_routes = "SELECT routes, routes_raw from $type WHERE mgmt_ip = \"$o_host\"";
+#my $execute_query_current_previous_routes = $connect -> query($query_current_previous_routes);
+#my @current_previous_routes_array = $execute_query_current_previous_routes -> fetchrow;
+# ################################################################################################
+
+if($type eq 'C'){$standard = "Celcius";};
+if($type eq 'F'){$standard = "Farenheit";};
+
+if( -d "/temp/$folder" ){
+	print "";
+}else {
+      print "connection to $zone sensor is offline. cannot find /temp/$folder\n";
+      exit $ERRORS{"UNKNOWN"};
+}
+
+my $filename = "/temp/$folder/w1_slave";
+if($debug eq "yes"){ print "filename = $filename\nmin = $minimum, max = $maximum, type = $type, standard = $standard\n"}
+
+my $checkFile = undef;
+
+my $data = undef;
+my $loop = 0;
+do{
+open(CURRENT, "< $filename") or die "Could not open file '$filename' $!";
+{
+        local $/;  # read all contents of opened file
+        $checkFile = <CURRENT>;  # place contents in this variable
+}
+close CURRENT;
+my @checkFileArray = split(" ",$checkFile);
+	if($debug eq "yes"){ 
+		for($a=0;$a<@checkFileArray;$a++){
+			print "checkFileArray[$a] = $checkFileArray[$a]\n"; 
+		}
+	}
+
+my $size = @checkFileArray;
+$data = $checkFileArray[$size-1];  # all we need is the last array value
+if($debug eq "yes"){ print "data1 = $data\n"}
+
+# filter string
+$data =~ s/t=//g;
+if($debug eq "yes"){ print "data2 = $data\n"}
+
+if( ($data < -20 && $data > 80000) || $data == 0 ){ $loop = $loop +1; }else{ $loop = 15; }
+} until($loop==15);
+
+# quit if sensor is offline
+if( ($data < -20 && $data > 80000) || $data == 0 ){
+        print "connection to $zone sensor is offline.\n";
+        exit $ERRORS{"UNKNOWN"};
+}
+if( $data == 0 && $checkFileArray[11] eq "NO" ){
+        print "connection to $zone sensor is offline.\n";
+        exit $ERRORS{"UNKNOWN"};
+}
+
+
+
+# add decimal point
+######################################
+$data = $data/1000;
+my $Cdata = round($data);
+	if($debug eq "yes"){ print "data3 = $data\n\n"}
+
+my $Fdata = (9 * $data/5) + 32;
+	if($debug eq "yes"){ print "Fdata = $Fdata\nCdata = $Cdata\n\n"}
+$Fdata = round($Fdata);
+	if($debug eq "yes"){ print "Fdata = $Fdata\nCdata = $Cdata\n\n"}
+
+#########################################
+my $use = undef;
+if($standard eq "Farenheit"){ $use = $Fdata }
+if($standard eq "Celcius"){ $use = $Cdata }
+my $MinWarningLevel = $minimum-5;
+my $MaxWarningLevel = $maximum+5;
+	if($debug eq "yes"){ print "use = $use\nwarning level = $MinWarningLevel\nMaxWarningLevel = $MaxWarningLevel\n "}
+
+
+if($process eq "nagios"){
+
+if( $use > $minimum  && $use < $maximum ){
+		print "Temperature: $use degrees $standard. Threshold:  $minimum-$maximum degrees $standard.   Everything is ok.\n";
+		exit $ERRORS{"OK"};	
+	}elsif( $use > $MinWarningLevel && $use <= $minimum ){
+		print "WARNING-Temperature: $use degrees $standard. Temperature is outside desired range!  Please check $zone kennel.\n";
+		exit $ERRORS{"WARNING"};	
+	}elsif( $use > $maximum && $use <= $MaxWarningLevel ){
+		print "WARNING-Temperature: $use degrees $standard. Temperature is outside desired range!  Please check $zone kennel.\n";
+		exit $ERRORS{"WARNING"};
+	}elsif( $use > $MaxWarningLevel || $use < $MinWarningLevel ){
+		print "CRITICAL-Temperature: $use degrees $standard. Temperature is way outside desired range!  Please check $zone kennel.\n";	
+		exit $ERRORS{"CRITICAL"};	
+	}else{
+		print "something went wrong!\n";
+		exit $ERRORS{"UNKNOWN"};	
+	}
+}else{
+
+	print "$use ";
+}
+
+
+
+
+
